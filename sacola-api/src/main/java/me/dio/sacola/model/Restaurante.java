9951d41ea94cfeb5c2e40d946d4121080bf529d4
Leandro <leandrogerolim@gmail.com>
2022-09-28 20:22:29 -0400

    finalizado criação da api sem o banco de dados.

---
+++ sacola-api/src/main/java/me/dio/sacola/model/Restaurante.java
@@ -0,0 +1,33 @@
+package me.dio.sacola.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@AllArgsConstructor
+@Builder
+@Data
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
+@Entity
+
+public class Restaurante {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Produto> cardapio;
+    @Embedded
+    private Endereco endereco;
+
+}
