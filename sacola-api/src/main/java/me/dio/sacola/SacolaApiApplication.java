0378f07bc6c3b717ebd446d595ca7589aaf1a641
Leandro <leandrogerolim@gmail.com>
2022-10-03 19:54:56 -0400

    add comentario.

--- sacola-api/src/main/java/me/dio/sacola/SacolaApiApplication.java
+++ sacola-api/src/main/java/me/dio/sacola/SacolaApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SacolaApiApplication {
-
+// o teste é feito na main
 	public static void main(String[] args) {
 		SpringApplication.run(SacolaApiApplication.class, args);
 	}
